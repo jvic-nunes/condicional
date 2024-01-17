2ca57a01be26b13b7dcabd61e3935d528ac08102
jv-nunes <jvnlino@gmail.com>
2024-01-16 21:28:24 -0300

    modificado

--- exerc-13.js
+++ exerc-13.js
@@ -1,14 +1,5 @@
 let salario = 100
 
-/* if (salario <= 500.00){    
-    console.log((salario * (5/100)) + salario)
-} else if (salario >= 500.01 && salario < 1200.00){    
-    console.log((salario * (12/100)) + salario)
-} else {
-    console.log(salario)
-} */
-
-
 if (salario <= 600.00){    
     if (salario <= 500.00){    
         console.log((salario * (5/100)) + salario + 150)
@@ -19,11 +10,10 @@ if (salario <= 600.00){
     }
 
 }else {    
-    if (salario <= 500.00){    
-        console.log((salario * (5/100)) + salario + 600)
-    } else if (salario >= 500.01 && salario < 1200.00){    
+    if (salario <= 1200.00){    
         console.log((salario * (12/100)) + salario + 600)
     } else {
         console.log(salario + 600)
     }
 }
+//opaaaaa teste
