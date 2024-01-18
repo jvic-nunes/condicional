bff4c97f72809be5e7686626633b9065dd692b0e
jv-nunes <jvnlino@gmail.com>
2024-01-18 10:49:45 -0300

    pedra pepel tesoura lagarto spock

---
+++ exerc-14.js
@@ -0,0 +1,64 @@
+/* Duas pessoas estão jogando pedra, papel ou tesoura. 
+Você deve imprimir o nome da jogada vencedora, ou "empate", em caso de empate. */
+
+
+let p1 = "lagarto" 
+let p2 = "spock"
+
+if(p1 === p2){
+    console.log("Empate")
+}else if(
+    (p1 === "tesoura" && p2 === "papel") ||
+    p1 === "tesoura" && p2 === "lagarto" ||
+    p1 === "papel" && p2 === "pedra" ||
+    p1 === "papel" && p2 === "spock" ||
+    p1 === "pedra" && p2 === "lagarto" ||
+    p1 === "pedra" && p2 === "tesoura" ||
+    p1 === "lagarto" && p2 === "spock" ||
+    p1 === "lagarto" && p2 === "papel" ||
+    p1 === "spock" && p2 === "tesoura" ||
+    p1 === "spock" && p2 === "pedra"
+    ){
+    console.log(p1 + " É o vencedor!")
+}else{
+    console.log(p2 + " É o vencedor!")
+}
+
+
+
+
+/* Scissors cuts Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitates Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+(and as it always has) Rock crushes Scissors */
+
+
+
+
+
+
+
+/* let v = "ganha"
+let p = "perde"
+
+if (p1 === p2){
+    console.log("Empate")
+    
+    
+    if(p1 === tesoura && p2 === papel){
+        console.log(`tesoura ${v}`)
+    }else if(p1 === papel && p2 === tesoura){
+        console.log(`papel ${p}`)
+    }if(p1 === papel && p2 === pedra){
+        console.log(`papel ${v}`)
+        }else{
+        console.log(`pedra ${p}`)
+        }    
+}  */ 
+
---
+++ exerc-15.js
@@ -0,0 +1,59 @@
+/* Duas pessoas estão jogando par ou ímpar. 
+Você deve imprimir "par" caso a vencedora seja a pessoa que escolheu par 
+e "ímpar" caso a vencedora seja a pessoa que escolheu ímpar. */
+
+/* let p1 = 2
+let p2 = 3
+let escolha1 = "par"
+let escolha2 = "impar"
+
+
+
+if(p1 === "par"){
+    p1 % 2 === 0
+}else if (p1 === "impar"){
+    p1 % 2 !== 0
+}else if (p2 === "par"){
+    p2 % 2 === 0
+}else {
+    p2 % 2 !== 0
+}
+
+if(p1 === p2){
+    console.log("etapme")    
+}else if (p1 + p2 === escolha1){
+    console.log("player 1 ganhou!")
+}else if (p1 + p2 === escolha1){
+    console.log("player 1 ganhou!")
+}else if (p1 + p2 === escolha2){
+    console.log("Player 2 ganhou!")
+}else if (p1 + p2 === escolha2){
+    console.log("Player 2 ganhou!")
+}
+    
+ */
+  
+
+//if(p1 % 2 === 0 || p2 % 2 === 0)
+
+let escolha = 1
+let p1 = 7
+let p2 = 7
+let soma = p1 + p2
+
+
+/* if (escolha === 1){
+    p2 = "par"
+    p1 = "impar"
+}else{
+    p1 = "par"
+    p2 = "impar"
+} */
+
+if(soma % 2 === 0){
+    console.log("O player par ganhou!!")
+}else {
+    console.log("o player impar ganhou!!")
+}
+
+// o foda é que o n consigo dizer qual player ganhou
